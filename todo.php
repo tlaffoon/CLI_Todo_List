ca3be628087e62c84fe2cc1f510fe394a9263c29
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-16 14:07:37 -0500

    Updated to include functions.

--- todo.php
+++ todo.php
@@ -3,46 +3,65 @@
 // Create array to hold list of todo items
 $items = array();
 
-// Main Loop
-do {
+// List array items formatted for CLI
+function list_items($list)
+{
+    // Return string of list items separated by newlines.
+    // Should be listed [KEY] Value like this:
+    // [1] TODO item 1
+    // [2] TODO item 2
+    // DO NOT USE ECHO, USE RETURN
 
-    // Iterate through list items
-    foreach ($items as $item_num => $item_value) {
+     foreach ($list as $key => $item) {
         // Display each item and a newline
-        echo "[{$item_num}] {$item_value}\n";
-    }
+        $output .= "[{$key}] {$item}" . PHP_EOL;   
+     } 
+        return $output;
+}
+
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = FALSE) 
+{   
+
+    $input = trim(fgets(STDIN));
+    // Return filtered STDIN input
+    return $upper ? strtoupper($input) : $input;
+}
+
+// The loop!
+do {
+    // Echo the list produced by the function
+    echo list_items($items);
 
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = trim(fgets(STDIN));
+    $input = get_input(TRUE);
 
     // Check for actionable input
-    if ($input == 'N' || $input == 'n') {
+    if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
-
-    } elseif ($input == 'R' || $input == 'r') {
+        $items[] = get_input();
+    } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = trim(fgets(STDIN));
+        $key = get_input();
         // Remove from array
-        unset($items[$item_num]);
-
+        unset($items[$key]);
     }
-    // This code unshifts the array to list items starting with 1, instead of 0.
-    // array_unshift
-    array_unshift($items,"");
-    // unset
+
+    // Re-order list each time, dropping 0 key.
+    array_unshift($items, "");
     unset($items[0]);
 
 // Exit when input is (Q)uit
-} while ($input != 'Q' && $input != 'q');
+} while ($input != 'Q');
 
 // Say Goodbye!
 echo "Goodbye!\n";
