c5e5a75fcaed9dabf459e4455bc357f02f340ded
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-20 09:15:01 -0500

    Added (B) (E) options for new items.

--- todo.php
+++ todo.php
@@ -1,11 +1,12 @@
 <?php
+/* -------------------------------------- */
 
 // Create array to hold list of todo items
 $items = array();
 
-// List array items formatted for CLI
+/* -------------------------------------- */
+
 function list_items($list) {
-    // Return string of list items separated by newlines.
 
     $output = '';
 
@@ -16,13 +17,15 @@ function list_items($list) {
         return $output;
 }
 
-// Get STDIN, strip whitespace and newlines, 
-// and convert to uppercase if $upper is true
+/* -------------------------------------- */
+
 function get_input($upper = FALSE) {   
     $input = trim(fgets(STDIN));
     // Return filtered STDIN input
     return $upper ? strtoupper($input) : $input;
 }
+
+/* -------------------------------------- */
 
 function sort_menu($list, $sort_type) {
     switch ($sort_type) {
@@ -49,6 +52,8 @@ function sort_menu($list, $sort_type) {
     return $list;
 }
 
+/* -------------------------------------- */
+
 // The loop!
 do {
     // Echo the list produced by the function
@@ -60,12 +65,31 @@ do {
     // Get the input from user
     $input = get_input(TRUE);
 
+/* -------------------------------------- */
+
     // Check for actionable input
     if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
-        // Add entry to list array
-        $items[] = get_input();
+        $item = get_input();
+
+    // Prompt user for beginning or end
+            echo '(B)eginning or (E)nd ?  ';
+            $input = get_input(TRUE);
+
+        if ($input == 'B') {
+            array_unshift($items, $item);
+        } 
+
+        elseif ($input == 'E') {
+            array_push($items, $item);
+        } 
+
+        else 
+            // Add entry to list array
+            $items[] = get_input();
+
+/* -------------------------------------- */
 
     } elseif ($input == 'R') {
         // Remove which item?
@@ -77,11 +101,15 @@ do {
         // Re-order numerical index of array
         $items = array_values($items);
 
+/* -------------------------------------- */
+
     } elseif ($input == 'S') {
         echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered: ";
         $sort_type = get_input(TRUE);
         $items = sort_menu($items, $sort_type);
     }
+
+/* -------------------------------------- */
 
 // Exit when input is (Q)uit
 } while ($input != 'Q');
