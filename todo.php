efa668fba9a609d5fed967a777cd8d32c9c89b67
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-21 01:10:37 -0500

    Modified logic and moved code into separate functions.

--- todo.php
+++ todo.php
@@ -1,29 +1,7 @@
 <?php
 
 // Create an empty array to hold list of todo items
-$items = array();
-
-/* -------------------------------------- */
-/* LIST ITEMS
-
-  This function takes the current list array as input, and 
-loops over the items in it to change the array to a string.
-
-  It then returns a string which is the list of the items. */
-
-function list_items($list) {
-
-    // Initialize empty string to eliminate php notice error.
-    $output = '';
-
-    // Loop over array items and construct string $output
-    foreach ($list as $key => $item) {
-        // Display each item and a newline
-        $output .= "[" . ($key + 1) . "] $item" . PHP_EOL;   
-     } 
-        // Return string $output as a product of this function.
-        return $output;
-}
+$list = array();
 
 /* -------------------------------------- */
 /* GET USER INPUT
@@ -42,13 +20,95 @@ function get_input($upper = FALSE) {
 }
 
 /* -------------------------------------- */
+
+/* LIST ITEMS
+
+  This function takes the current list array as input, and 
+loops over the items in it to change the array to a string.
+
+  It then returns a string which is the list of the items. */
+
+function output_list($list) {
+
+    // Initialize empty string to eliminate php notice error.
+    $output = '';
+
+    // Loop over array items and construct string $output
+    foreach ($list as $key => $item) {
+        // Display each item and a newline
+        $output .= "[" . ($key + 1) . "] $item" . PHP_EOL;   
+     } 
+        // Return string $output as a product of this function.
+        return $output;
+}
+
+/* -------------------------------------- */
+
+//  ADD ITEM
+
+function add_item($list) {
+
+    // Ask for entry
+    echo 'Enter item: ';
+    $item = get_input();
+
+    // Prompt user for beginning or end
+    echo 'Place new items at (B)eginning or (E)nd of current list? ';
+    $location = get_input(TRUE);
+
+    // Logic for placing at beginning of list.
+    if ($location == 'B') {
+        array_unshift($list, $item);
+    } 
+
+    // Default option is just to append item to current list.
+    else {
+        // Add entry to list array
+        array_push($list, $item);
+    }
+
+    return $list;
+}
+
+/* -------------------------------------- */
+
+// REMOVE ITEM
+
+function remove_item($list) {
+
+    // Remove which item?
+    echo 'Enter item number to remove: ';
+
+    // Get array key
+    $key = get_input();
+ 
+    // Remove from array
+    unset($list[$key - 1]);
+ 
+    // Re-order numerical index of array
+    $list = array_values($list);
+
+    return $list;
+}
+
+function remove_first($list) {
+    unset($list[0]);
+    return array_values($list);
+}
+
+function remove_last($list) {
+    unset($list[count($list) - 1]);
+    return $list;
+}
+
+
+/* -------------------------------------- */
 /* SORT MENU
 
   This function takes in the current list and an option for sorting.
   It then returns the list sorted according to the type specified. */
 
-function sort_menu($list, $sort_type) {
-
+function sort_list($list, $sort_type) {
 
     // Switch statement $sort_type tells us which sort to use.
     switch ($sort_type) {
@@ -78,75 +138,43 @@ function sort_menu($list, $sort_type) {
 }
 
 /* -------------------------------------- */
-// BEGIN MAIN LOOP
+
+// ********** MAIN LOOP ********** // 
 
 do {
-    // Echo the list produced by the function
-    echo list_items($items);
+
+	// Output current list; if empty then blank.
+	echo output_list($list);
 
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (S)ort items, (Q)uit : ';
+	echo '(A)dd item, (R)emove item, (S)ort list, (Q)uit : ';
 
-    // Get the input from user
-    $input = get_input(TRUE);
+    // Calls get_input(); to get user menu choice.
+    $menu_choice = get_input(TRUE);
 
-/* -------------------------------------- */
-//  ADD ITEMS
-
-    // Check for actionable input
-    if ($input == 'N') {
-        // Ask for entry
-        echo 'Enter item: ';
-        $item = get_input();
-
-    // Prompt user for beginning or end
-            echo 'Place new items at (B)eginning or (E)nd of current list?  ';
-            $input = get_input(TRUE);
-
-        // Logic for placing at beginning of list.
-        if ($input == 'B') {
-            array_unshift($items, $item);
-        } 
-
-        // Logic for placing at end of list.
-        elseif ($input == 'E') {
-            array_push($items, $item);
-        } 
-
-        // Default option is just to append item to current list.
-        else 
-            // Add entry to list array
-            $items[] = get_input();
-
-/* -------------------------------------- */
-//  REMOVE ITEMS
-
-    } elseif ($input == 'R') {
-        // Remove which item?
-        echo 'Enter item number to remove: ';
-        // Get array key
-        $key = get_input();
-        // Remove from array
-        unset($items[$key - 1]);
-        // Re-order numerical index of array
-        $items = array_values($items);
-
-/* -------------------------------------- */
-//  SORT ITEMS
-
-    } elseif ($input == 'S') {
-        echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered: ";
-        $sort_type = get_input(TRUE);
-        $items = sort_menu($items, $sort_type);
+    switch ($menu_choice) {
+        case 'A':
+            $list = add_item($list);
+            break;
+        case 'R':
+            $list = remove_item($list);
+            break;
+        case 'S':
+            $list = sort_list($list);
+            break;
+        case 'F':
+            $list = remove_first($list);
+            break;        
+        case 'L':
+            $list = remove_last($list);
+            break;
     }
 
-/* -------------------------------------- */
-
 // Exit when input is (Q)uit
-} while ($input != 'Q');
+} while ($menu_choice != 'Q');
 
 // Say Goodbye!
-echo "Goodbye!\n";
+echo "Adios!\n";
 
 // Exit with 0 errors
 exit(0);
